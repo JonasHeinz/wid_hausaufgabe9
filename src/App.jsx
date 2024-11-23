6bcb70c30a119ed140ed5d0e5ec7678c8430ccca
Jonas Heinz <64084505+JonasHeinz@users.noreply.github.com>
2024-11-23 18:29:45 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Typography from '@mui/material/Typography';
+function App() {
+  <Typography variant="h1" gutterBottom>
+       Hello React +MUI!
+      </Typography>
+}
+
+export default App
